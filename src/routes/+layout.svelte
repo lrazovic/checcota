87bec7b85817a6e27d4edba6cfc1f13e3c41bc23
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2025-08-21 08:16:38 +0200

    feat: add Web Transition API

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,43 @@
 	import BaseLayout from '$lib/layouts/BaseLayout.svelte';
 
 	let { children } = $props();
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
-	<link rel="preload" as="font" href="/fonts/MonaspaceNeon-Bold.woff2" type="font/woff2" crossorigin="anonymous">
-	<link rel="preload" as="font" href="/fonts/MonaspaceNeon-Regular.woff2" type="font/woff2" crossorigin="anonymous">
-	<link rel="preload" as="font" href="/fonts/MonaspaceNeon-Italic.woff2" type="font/woff2" crossorigin="anonymous">
+	<link
+		rel="preload"
+		as="font"
+		href="/fonts/MonaspaceNeon-Bold.woff2"
+		type="font/woff2"
+		crossorigin="anonymous"
+	/>
+	<link
+		rel="preload"
+		as="font"
+		href="/fonts/MonaspaceNeon-Regular.woff2"
+		type="font/woff2"
+		crossorigin="anonymous"
+	/>
+	<link
+		rel="preload"
+		as="font"
+		href="/fonts/MonaspaceNeon-Italic.woff2"
+		type="font/woff2"
+		crossorigin="anonymous"
+	/>
 </svelte:head>
 
 <BaseLayout>
